d68188e60cc42178aa52c9473446fbacdd6bd541
Harsh Narayan Jha <harshnj@proton.me>
2025-11-28 14:19:54 +0530

    basic day night cycle with visible trailcrumbs

--- scripts/components/time_manager.gd
+++ scripts/components/time_manager.gd
@@ -5,6 +5,7 @@ extends Node2D
 @export var timer: Timer
 @export var second_duration: float = 1.0
 @export var debug_label: Label
+@export var night_light: Light2D
 
 @export var time_data: TimeData
 
@@ -29,12 +30,21 @@ func _ready() -> void:
 	else:
 		debug_label.queue_free()
 
+	time_data.day_time_changed.connect(handle_day_time_change)
+	handle_day_time_change(time_data.get_current_day_time(), time_data.get_current_day())
+
 func _tick() -> void:
 	time_data.tick_second()
 
 func finish() -> void:
 	timer.stop()
 	timer.timeout.disconnect(_tick)
+
+func handle_day_time_change(day_time: TimeData.DayTime, _day: int) -> void:
+	if day_time == TimeData.DayTime.DAY:
+		night_light.hide()
+	else:
+		night_light.show()
 
 func _exit_tree() -> void:
 	timer.timeout.disconnect(_tick)
